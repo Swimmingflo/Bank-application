56d7cc1fdf37c72155a31c43fc7b027df71562d0
Florian Bidaut <24437@student.dorset-college.ie>
2021-10-24 15:59:24 +0100

    Ajoutez des fichiers projet.

---
+++ Banking application/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Banking_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you a Bank Employee or a Customer?");
+            string response = Console.ReadLine();
+            if (response == "Bank Employee")
+            {
+                Console.WriteLine("Log in please");
+                string loginEmployee = Console.ReadLine();
+                if (loginEmployee == "A1234")
+                {
+                    Console.WriteLine("Welcome Bank Employee");
+                }
+                else
+                {
+
+                }
+            }
+            else if(response == "Customer")
+            {
+                Console.WriteLine("Log in please");
+                string loginCustomer = Console.ReadLine();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
